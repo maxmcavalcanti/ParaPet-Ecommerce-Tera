5acdfd8a1b1119f85aaaf0227deab806e10f1947
Maximiliano Muñoz Cavalcanti <95191637+maxmcavalcanti@users.noreply.github.com>
2022-07-09 15:36:08 -0300

    First Commit

---
+++ backend/routes/PetRoutes.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+
+const PetController = require("../controllers/PetController");
+
+//middlewares
+const verifyToken = require("../helpers/verify-token");
+const { imageUpload } = require("../helpers/image-upload");
+
+router.post(
+  "/create",
+  verifyToken,
+  imageUpload.array("images"),
+  PetController.create
+);
+router.get("/", PetController.getAll);
+router.get("/mypets", verifyToken, PetController.getAllUserPets);
+router.get("/myadoptions", verifyToken, PetController.getAllUserAdoptions);
+router.get("/:id", PetController.getPetById);
+router.delete("/:id", verifyToken, PetController.removePetById);
+router.patch(
+  "/:id",
+  verifyToken,
+  imageUpload.array("images"),
+  PetController.updatePetById
+);
+router.patch("/schedule/:id", verifyToken, PetController.schedule);
+router.patch("/myadoptions/:id", verifyToken, PetController.deleteProduct);
+router.patch("/cart/:id", verifyToken, PetController.updateCart);
+router.patch("/conclude/:id", verifyToken, PetController.concludeAdoption);
+
+module.exports = router;
